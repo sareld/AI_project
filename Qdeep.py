4d8b9ff0aa720b83907c04b9cca7c49f8057c184
naomideutsch <naomi.deutsch@mail.huji.ac.il>
2018-08-06 21:31:49 +0300

    Qdeep not working. adding QdeepCart for comparison

--- Qdeep.py
+++ Qdeep.py
@@ -1,16 +1,16 @@
-import random
-
-from Q import *
-
-import pickle
-import tensorflow as tf
 from collections import deque
+
+import tensorflow as tf
+import util
 import numpy as np
+import random
+# from Cart import CART_ACTIONS
+
 EMPTY_VAL = 0
 
 NUM_OF_LAYERS = 2
 LAYER_DEPTH = 5
-INPUT_SIZE = 3
+INPUT_SIZE = 2
 OUTPUT_SIZE = 3
 
 RANDOM_WEIGHTS = 0.05
@@ -23,7 +23,14 @@ GAMMA = 0.95
 EPSILON_DECAY = 0.8
 HISTORY_SIZE = 200
 
-class Qdeep(Q):
+
+RIGHT = 100
+LEFT = -100
+STAY = 0
+
+CART_ACTIONS = [LEFT,RIGHT,STAY]
+
+class Qdeep():
 
     def __init__(self):
         self.history = deque(maxlen=HISTORY_SIZE)  # to save the history of states, actions and rewards during the game
@@ -31,7 +38,8 @@ class Qdeep(Q):
 
         self.build_net()
 
-    def weight_variable(self,name, shape):
+
+    def weight_variable(self, name, shape):
         """Initialize weight variable
         Parameter
         ---------
@@ -41,8 +49,9 @@ class Qdeep(Q):
         -------
         The created `tf.get_variable` for weights.
         """
-        initial_value = tf.random_uniform(shape=shape,minval=-0.5,maxval=0.5, dtype="float64")
+        initial_value = tf.random_uniform(shape=shape, minval=-0.5, maxval=0.5, dtype="float64")
         return tf.get_variable(name=name, initializer=initial_value)
+
 
     def bias_variable(self, name, shape):
         """Initialize bias variable
@@ -57,18 +66,21 @@ class Qdeep(Q):
         initial_value = tf.constant([0.1], shape=shape, dtype="float64")
         return tf.get_variable(name=name, initializer=initial_value)
 
+
     def take(self, indices):
         """
         Function gets indices of the output vector and returns a mask of their values
         """
         mask = tf.one_hot(indices=indices, depth=OUTPUT_SIZE, dtype=tf.bool, on_value=True, off_value=False, axis=-1)
-        return tf.boolean_mask(self.outputs, mask)
+        x = tf.boolean_mask(self.outputs, mask)
+
+        return x
 
     def build_net(self):
         with tf.name_scope('input'):
             self.x_input = tf.placeholder(dtype=tf.float64, shape=[None, INPUT_SIZE], name='x_input')
-            self.action = tf.placeholder(dtype=tf.int64, shape=(None,), name="action")
-            self.q_est = tf.placeholder(dtype=tf.float64, shape=(None,), name="q_estimation")
+            self.action = tf.placeholder(dtype=tf.int64, shape=[None], name="action")
+            self.q_est = tf.placeholder(dtype=tf.float64, shape=[None], name="q_estimation")
 
         first_hidden_layer = {'weights': self.weight_variable('h1_w_layer', [INPUT_SIZE, LAYER_DEPTH]),
                               'biases': self.bias_variable('h1_b_layer', [LAYER_DEPTH])}
@@ -78,7 +90,7 @@ class Qdeep(Q):
                                'biases': self.bias_variable('h2_b_layer', [LAYER_DEPTH])}
 
         self.output_layer = {'weights': self.weight_variable('output_w_layer', [LAYER_DEPTH, OUTPUT_SIZE]),
-                        'biases': self.bias_variable('output_b_layer', [OUTPUT_SIZE])}
+                             'biases': self.bias_variable('output_b_layer', [OUTPUT_SIZE])}
 
         first_layer = tf.matmul(self.x_input, first_hidden_layer['weights']) + first_hidden_layer['biases']
         first_layer = tf.sigmoid(first_layer)
@@ -89,10 +101,11 @@ class Qdeep(Q):
         self.outputs = output_layer
         self.sess = tf.Session()
 
+
+
+        self.loss = tf.reduce_mean(tf.square(self.q_est[0] - self.take(self.action)[0]))
         print("q =", self.q_est)
         print("take =", self.take(self.action))
-
-        self.loss = tf.reduce_mean(tf.square(self.q_est - self.take(self.action)))
         self.optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
         self.train_op = self.optimizer.minimize(self.loss)
         self.sess.run(tf.global_variables_initializer())
@@ -127,12 +140,12 @@ class Qdeep(Q):
 
         for i in range(batch_size):
             prev_state, prev_action, reward, new_state, done = batch[i]
-            prev_states.append(np.array(prev_state).flatten())
+            prev_states.append(np.array([prev_state.angles, prev_state.angular_vel]).flatten())
             prev_actions.append(prev_action)
 
-            fd_v = {self.x_input: np.array(new_state).flatten().reshape([1, INPUT_SIZE])}
+            fd_v = {self.x_input: np.array([new_state.angles, new_state.angular_vel]).flatten().reshape([1, INPUT_SIZE])}
             v = np.max(self.sess.run(self.outputs, feed_dict=fd_v))
-            q = reward +  GAMMA * v
+            q = reward + GAMMA * v
             qs.append(q)
 
         fd = {}
@@ -142,17 +155,15 @@ class Qdeep(Q):
 
         self.sess.run(self.train_op, feed_dict=fd)
 
+
     def get_action(self, state):
         """
         Function gets a state and returns its the action that maximizes the Q function (from the legal actions)
         """
-        fd = {
-            self.x_input: state.flatten().reshape([1, INPUT_SIZE])
-        }
+        fd = {self.x_input: np.array([state.angles, state.angular_vel]).flatten().reshape([1, INPUT_SIZE])}
         net_out = self.sess.run(self.outputs, feed_dict=fd)
+        return CART_ACTIONS[np.argmax(net_out)]
 
-
-        return net_out
 
     def save_to_history(self, prev_state, prev_action, reward, new_state, done):
         """
@@ -160,7 +171,8 @@ class Qdeep(Q):
         """
         self.history.append((prev_state, prev_action, reward, new_state, done))
 
-    def act(self,  prev_state, prev_action, reward, new_state):
+
+    def act(self, prev_state, prev_action, reward, new_state):
         """
         the function for choosing an action, given current state.
         it accepts the state-action-reward needed to learn from the previous
@@ -183,8 +195,6 @@ class Qdeep(Q):
         if prev_state is not None:
             self.save_to_history(prev_rep, prev_action, reward, new_rep, 0)
         # e-greedy in train
-
-
         return self.get_action(new_state)
 
     def getQValue(self, state, action):
@@ -193,11 +203,13 @@ class Qdeep(Q):
           Should return 0.0 if we never seen
           a state or (state,action) tuple
         """
-        state = np.insert(state,0,[1])
-        a=self.get_action(state)
-        return np.max(a)
+        # state = np.insert(state, 0, [1])
 
-    def update(self, state, action, nextAction, nextState, reward):
+        fd = {self.x_input: np.array([state.angles, state.angular_vel]).flatten().reshape([1, INPUT_SIZE])}
+        net_out = self.sess.run(self.outputs, feed_dict=fd)
+        return net_out[0][CART_ACTIONS.index(action)]
+
+    def update(self, state, action, nextAction, nextState, reward, legal_moves):
         """
           The parent class calls this to observe a
           state = action => nextState and reward transition.
@@ -206,7 +218,7 @@ class Qdeep(Q):
           NOTE: You should never call this function,
           it will be called on your behalf
         """
-        #self.act(state,action,reward,nextState)
-        state = np.insert(state,0,[1])
-        nextState = np.insert(nextState,0,[1])
-        self.learn(state,action,reward,nextState)
+        # self.act(state,action,reward,nextState)
+        # state = np.insert(state, 0, [1])
+        # nextState = np.insert(nextState,0,[1])
+        self.learn(state, action, reward, nextState)
